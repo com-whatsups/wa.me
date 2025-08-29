95d0c7ee914844513e1fdd4507e871f2e832a1f2
com-whatsups <ahmadfarhanfuadi123@gmail.com>
2025-08-30 00:31:55 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-const redirectLink = "https://www.effectivecpmrate.com/ji133660?key=0868c0542426da7d87a0884de9a08f04";
-let time = 3;
-const timerEl = document.getElementById("timer");
+let timer = 3;
+const countdown = document.getElementById('timer');
+const adsterraLink = "https://www.effectivecpmrate.com/ji133660?key=0868c0542426da7d87a0884de9a08f04";
 
 const interval = setInterval(() => {
-  time--;
-  timerEl.textContent = time;
-  if(time <= 0){
+  timer--;
+  countdown.textContent = timer;
+  if (timer <= 0) {
     clearInterval(interval);
-    window.location.href = redirectLink;
+    redirectNow();
   }
 }, 1000);
+
+function redirectNow() {
+  window.location.href = adsterraLink;
+}
